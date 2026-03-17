debf369b2fce1108510ea1fdd177385a071770f0
Yashraj Sahu <yashrajjjsahu@gmail.com>
2026-03-17 23:40:55 +0530

    Add files via upload

---
+++ Linear search/Searching.java
@@ -0,0 +1,6 @@
+// Searching means finding whether an elemnet (called target) exists in a given data structure like an array,Sttring,or list.
+
+// Types of searching
+
+// 1.Linear search - Check each element on by one
+// 2.Binary search - Divide the arrays in halves (works only on sorted array)
